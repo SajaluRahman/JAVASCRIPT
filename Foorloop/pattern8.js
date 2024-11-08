1d1bc5fad200b792796bde30e1c3f54c03f3c38a
SajaluRahman <sajalurahman321@gmail.com>
2024-11-08 09:53:13 +0530

    pattern8

---
+++ Foorloop/pattern8.js
@@ -0,0 +1,17 @@
+let a = 5; 
+for (let i = 1; i <= a; i++) {
+    let b = "";
+    for (let c = 1; c <= a - i; c++) {
+        b += " "; 
+    }
+    for (let d = 1; d <= i; d++) {
+        b += "* ";
+    }
+    console.log(b);
+
+}
+
+z
+
+
+
