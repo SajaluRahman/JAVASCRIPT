e57ae41a79c15a03e6cc682d904a1dd14fc0671a
SajaluRahman <sajalurahman321@gmail.com>
2024-11-14 09:55:43 +0530

    pattern10

---
+++ Foorloop/pattern10.js
@@ -0,0 +1,12 @@
+let a = 5; 
+for (let i = a; i >= 1; i--) {
+    let b = "";
+    for (let c = 1; c <= a - i; c++) {
+        b += " "; 
+    }
+    for (let d = 1; d <= i; d++) {
+        b += "* ";
+    }
+    console.log(b);
+
+}
