74570c4d06394db7ac8168e38285b3e43296d2bc
SajaluRahman <sajalurahman321@gmail.com>
2024-11-15 10:06:50 +0530

    pattern11

---
+++ Foorloop/pattern11.js
@@ -0,0 +1,13 @@
+let z = 3; 
+
+for (let i = 1; i <= z; i++) {
+    let b = ""; 
+    for (let d = 1; d <= i; d++) {
+              b += " ";
+            }
+    for (let c = 2; c <= 5; c++) {
+        b += "* "; 
+    }
+    
+    console.log(b);
+}
